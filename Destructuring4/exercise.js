2ba2aba6f29059eda8e3be400e6dfefe6890dc59
Jeniffer Flores <hachikofu03@gmail.com>
2023-08-26 18:43:21 +0200

    ejercicios 23 al 26

--- Destructuring4/exercise.js
+++ Destructuring4/exercise.js
@@ -1,8 +1,15 @@
-const person = {
+/* const person = {
   id: 1,
   firstName: 'Mario',
   lastName: 'Rossi',
   age: 25
+}; */
+
+const person = {
+  id: 1,
+  name: 'Mario',
+  surname: 'Rossi',
+  old: 25
 };
 
 const { id, name, surname, old } = person;
