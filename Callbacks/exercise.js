51197c55fb575ee107dec4feca2102e767a372c8
Jeniffer Flores <hachikofu03@gmail.com>
2023-09-04 04:34:58 +0200

    ejercicios 34,35 y 36

--- Callbacks/exercise.js
+++ Callbacks/exercise.js
@@ -1,1 +1,6 @@
+function printAsyncName (callback, name) {
+setTimeout (callback, 1000);
+setTimeout (name,2000);
+}
 
+printAsyncName (() => console.log("Hello"), () => console.log ("Julio"))
--- Callbacks2/exercise.js
+++ Callbacks2/exercise.js
@@ -1,1 +1,9 @@
+function repeatHello (callback/* ,n*/)  {
+   /*  let id = */ setInterval (callback,1000);
+    /* setTimeout (()=> clearInterval (id),1000*n) */
+};
+ repeatHello (()=> console.log("Hello"));
 
+ // Las arrow functions son preferibles por varios motivos
+ // pero aqui es porque tiene una sintaxis más concisa 
+ // que otros tipos de funciones
