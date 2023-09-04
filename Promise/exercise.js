87fda1f5771f41b46e8de762400cadabb07afc2d
Jeniffer Flores <hachikofu03@gmail.com>
2023-09-04 07:41:00 +0200

    ejercicio 37

--- Promise/exercise.js
+++ Promise/exercise.js
@@ -1,1 +1,12 @@
 const number = 15;
+
+let promise = new Promise ((resolve, reject) => {
+    if (number > 10) {
+        resolve (number);
+    } else {
+        reject (`${number} is too small`);
+    }
+});
+
+promise.then ((number) => console.log(number));
+promise.catch ((err) => console.log(err));
