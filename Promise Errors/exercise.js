1502587fb65ab4f90611c97b88cea663f4cfa7fe
Jeniffer Flores <hachikofu03@gmail.com>
2023-09-05 20:17:14 +0200

    ejercicio 38

--- Promise Errors/exercise.js
+++ Promise Errors/exercise.js
@@ -1,1 +1,58 @@
+class notLoggedIn extends Error {
+  constructor(err) {
+    super(err);
+    this.name = "NotLoggedInError";
+  }
+}
 
+class RandomNumError extends Error {
+  constructor(err) {
+    super(err);
+    this.name = "RandomNumberError";
+  }
+}
+
+const isLogged = true;
+let randomNum = Math.random();
+
+function promise() {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      if (isLogged == true) {
+        resolve(console.log(randomNum));
+      } else {
+        reject(
+          new notLoggedIn()
+        );
+      }
+    }, 1000);
+  });
+}
+
+function promise2() {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      if (randomNum > 0.5) {
+        let obj = { name: "John", age: 24 };
+        resolve(console.log(obj));
+      } else {
+        reject(
+          new RandomNumError()
+        );
+      }
+    }, 1000);
+  });
+}
+
+promise()
+  .then(promise2)
+  .catch((err) => {
+    if (err instanceof notLoggedIn) {
+      console.log("User is not logged in.");
+    } else if (err instanceof RandomNumError) {
+      console.log("Random number is not greater than 0.5.");
+    } else {
+      console.log("An unknown error occurred.");
+    }
+  })
+  .finally(() => console.log("Promise chaining, end here"));
